22d762fbf4d39319a1580a02a141f1db10493663
Brian Xiong <bxiong1000@gmail.com>
2019-05-02 19:20:52 -0400

    To show git process

--- OpheliasOasis/OpheliasOasis/Program.cs
+++ OpheliasOasis/OpheliasOasis/Program.cs
@@ -21,10 +21,6 @@ namespace OpheliasOasis
             try
             {
                 DatabaseManager.loginUser("djones7777", "password1");
-                //var rsrvTable = DatabaseManager.findReservation("", DateTime.Now, DateTime.Now.AddDays(1));
-                //var tbl = DatabaseManager.getDailyOccupancyReport();
-                //DatabaseManager.modifyUser((int)tbl.Rows[0].ItemArray[0], "test", 3, "Tester", "Dusty", 10000);
-            }
             catch (Exception e)
             {
                 return;
