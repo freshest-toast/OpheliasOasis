07c2faaa20cd1fdfc0f90671a467f95a6e46db35
davidjones7777 <49825804+davidjones7777@users.noreply.github.com>
2019-05-02 21:22:58 -0400

    Updated database manager

--- OpheliasOasis/OpheliasOasis/Program.cs
+++ OpheliasOasis/OpheliasOasis/Program.cs
@@ -21,6 +21,13 @@ namespace OpheliasOasis
             try
             {
                 DatabaseManager.loginUser("djones7777", "password1");
+                var currReser = DatabaseManager.findReservation("Gravel", DateTime.Now, DateTime.Now);
+                DatabaseManager.checkIn(currReser.Rows[0].ItemArray[0] as string);
+                var reservations = DatabaseManager.getDailyOccupancyReport();
+                
+                DatabaseManager.changeReservation((string)reservations.Rows[0].ItemArray[0], DateTime.Now.AddMonths(1),
+                    DateTime.Now.AddMonths(2), "David", "Bones", null);
+                DatabaseManager.changeRate(DateTime.Now.AddDays(1), DateTime.Now.AddDays(1),(decimal)3.0);
                 //var rsrvTable = DatabaseManager.findReservation("", DateTime.Now, DateTime.Now.AddDays(1));
                 //var tbl = DatabaseManager.getDailyOccupancyReport();
                 //DatabaseManager.modifyUser((int)tbl.Rows[0].ItemArray[0], "test", 3, "Tester", "Dusty", 10000);
